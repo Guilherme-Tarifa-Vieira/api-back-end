3894656d58277eb58d31401ba45161454f83ebe1
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-08-16 17:35:15 -0300

    feat: create userform

---
+++ src/main/java/com/stack/apibooklovers/domain/user/UserForm.java
@@ -0,0 +1,26 @@
+package com.stack.apibooklovers.domain.user;
+
+import com.stack.apibooklovers.enums.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+
+public class UserForm {
+
+    @NotBlank
+    @Size(min = 3, max = 155)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 3, max = 155)
+    private String lastName;
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+    private Role role;
+
+}
