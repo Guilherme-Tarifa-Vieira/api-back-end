8dbe5405494655590dab47a624ebb8b6e8a2a563
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-10-09 08:37:00 -0300

    feat: pagination user

--- src/main/java/com/stack/apibooklovers/controller/UserController.java
+++ src/main/java/com/stack/apibooklovers/controller/UserController.java
@@ -4,6 +4,12 @@ import com.stack.apibooklovers.domain.user.UserForm;
 import com.stack.apibooklovers.domain.user.UserResponseDTO;
 import com.stack.apibooklovers.service.userService.UserService;
 import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,8 +27,10 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> getAllUser() {
-        return ResponseEntity.ok(userService.getAllUsers().getBody());
+    public ResponseEntity<Page<UserResponseDTO>> getAllUser(@ParameterObject @PageableDefault(size = 20, sort = "firstName", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userService.getAllUsers(pageable));
     }
 
 
@@ -33,7 +41,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserForm userForm) {
-        return ResponseEntity.status(201).body(userService.createUser(userForm).getBody());
+        return ResponseEntity.status(201).body(userService.createUser(userForm));
     }
 
 
