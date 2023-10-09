e5a4a90bfb9705fe4f307c25750e8989d15c2605
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-10-09 08:40:01 -0300

    refactor: refactoring code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,7 @@ CREATE TABLE tb_user
     email      VARCHAR(120)          NOT NULL,
     password   VARCHAR(120)          NOT NULL,
     role       VARCHAR(55)           NOT NULL,
-    CONSTRAINT pk_user PRIMARY KEY (ID)
+    CONSTRAINT pk_user PRIMARY KEY (id)
 
 );
 
@@ -15,7 +15,7 @@ CREATE TABLE tb_author
     id        BIGINT AUTO_INCREMENT NOT NULL,
     name      VARCHAR(155)          NOT NULL,
     birth_day DATE                  NULL,
-    CONSTRAINT pk_author PRIMARY KEY (ID)
+    CONSTRAINT pk_author PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_book
@@ -24,7 +24,7 @@ CREATE TABLE tb_book
     title  VARCHAR(155)          NOT NULL,
     isbn   VARCHAR(55)           NOT NULL,
     status VARCHAR(30)           NULL,
-    CONSTRAINT pk_book PRIMARY KEY (ID)
+    CONSTRAINT pk_book PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_book_user_favorites
@@ -47,27 +47,27 @@ ALTER TABLE tb_book
 
 
 
-INSERT INTO tb_author(id, name, birth_day)
-VALUES (1, 'Stephen King', '2018-07-07');
-INSERT INTO tb_author(id, name, birth_day)
-VALUES (2, 'J.K. Rolling', '2018-07-07');
-INSERT INTO tb_author(id, name, birth_day)
-VALUES (3, 'Georgie R.R. Martin', '2018-07-07');
-INSERT INTO tb_author(id, name, birth_day)
-VALUES (4, 'Rick Riordan', '2018-07-07');
-INSERT INTO tb_author(id, name, birth_day)
-VALUES (5, 'James Dashner', '2018-07-07');
+INSERT INTO tb_author(name, birth_day)
+VALUES ('Stephen King', '2018-07-07');
+INSERT INTO tb_author(name, birth_day)
+VALUES ('J.K. Rolling', '2018-07-07');
+INSERT INTO tb_author(name, birth_day)
+VALUES ('Georgie R.R. Martin', '2018-07-07');
+INSERT INTO tb_author(name, birth_day)
+VALUES ('Rick Riordan', '2018-07-07');
+INSERT INTO tb_author(name, birth_day)
+VALUES ('James Dashner', '2018-07-07');
 
-INSERT INTO tb_user(id, first_name, last_name, email, password, role)
-VALUES (1, 'Guilherme', 'Tarifa', 'Vieira@gmail.gloob', '1234554', 'MANAGER');
+INSERT INTO tb_user(first_name, last_name, email, password, role)
+VALUES ('Guilherme', 'Tarifa', 'Vieira@gmail.gloob', '1234554', 'MANAGER');
 
-INSERT INTO tb_book(id, title, author_id, isbn, status)
-VALUES (1, 'Salem', 1, '23124132.32123', 'READING');
-INSERT INTO tb_book(id, title, author_id, isbn, status)
-VALUES (2, 'Harry Potter e a Câmara Secreta', 2, '23111111.31111', 'TO_READ');
-INSERT INTO tb_book(id, title, author_id, isbn, status)
-VALUES (3, 'A Dança dos Dragões', 3, '222224132.322222', 'READ');
-INSERT INTO tb_book(id, title, author_id, isbn, status)
-VALUES (4, 'Filho de Netuno', 4, '333333333.333333', 'READ');
-INSERT INTO tb_book(id, title, author_id, isbn, status)
-VALUES (5, 'Maze Runner', 5, '2344443211.32444', 'TO_READ');
+INSERT INTO tb_book(title, author_id, isbn, status)
+VALUES ('Salem', 1, '23124132.32123', 'READING');
+INSERT INTO tb_book(title, author_id, isbn, status)
+VALUES ('Harry Potter e a Câmara Secreta', 2, '23111111.31111', 'TO_READ');
+INSERT INTO tb_book(title, author_id, isbn, status)
+VALUES ('A Dança dos Dragões', 3, '222224132.322222', 'READ');
+INSERT INTO tb_book(title, author_id, isbn, status)
+VALUES ('Filho de Netuno', 4, '333333333.333333', 'READ');
+INSERT INTO tb_book(title, author_id, isbn, status)
+VALUES ('Maze Runner', 5, '2344443211.32444', 'TO_READ');
