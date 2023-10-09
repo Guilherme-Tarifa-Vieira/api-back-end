64a74a8b9c725ca185da135f6f182eeb4bc9c7b8
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-10-09 08:37:22 -0300

    feat: pagination user

--- src/main/java/com/stack/apibooklovers/service/userService/UserService.java
+++ src/main/java/com/stack/apibooklovers/service/userService/UserService.java
@@ -2,18 +2,18 @@ package com.stack.apibooklovers.service.userService;
 
 import com.stack.apibooklovers.domain.user.UserForm;
 import com.stack.apibooklovers.domain.user.UserResponseDTO;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface UserService {
 
-    ResponseEntity<List<UserResponseDTO>> getAllUsers();
+    Page<UserResponseDTO> getAllUsers(Pageable pageable);
 
     UserResponseDTO getUserById(Long id);
 
 
-    ResponseEntity<UserResponseDTO> createUser(UserForm user);
+    UserResponseDTO createUser(UserForm user);
 
     UserResponseDTO addBookToFavorites(Long userId, Long bookId);
 }
--- src/main/java/com/stack/apibooklovers/service/userService/UserServiceImpl.java
+++ src/main/java/com/stack/apibooklovers/service/userService/UserServiceImpl.java
@@ -3,11 +3,15 @@ package com.stack.apibooklovers.service.userService;
 import com.stack.apibooklovers.domain.user.User;
 import com.stack.apibooklovers.domain.user.UserForm;
 import com.stack.apibooklovers.domain.user.UserResponseDTO;
-import com.stack.apibooklovers.exception.ConflictEmail;
-import com.stack.apibooklovers.exception.NoContentList;
-import com.stack.apibooklovers.exception.UserByIdNotFound;
-import com.stack.apibooklovers.mapper.Mapper;
+import com.stack.apibooklovers.infraestructure.exception.ConflictEmail;
+import com.stack.apibooklovers.infraestructure.exception.NoContentList;
+import com.stack.apibooklovers.infraestructure.exception.UserByIdNotFound;
 import com.stack.apibooklovers.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -19,37 +23,40 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
 
-    private final UserRepository userRepository;
+    private UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
-    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
-        List<User> users = userRepository.findAll();
+    public Page<UserResponseDTO> getAllUsers(Pageable pageable) {
+        Page<User> users = userRepository.findAll(pageable);
         if (users.isEmpty()) throw new NoContentList("Lista vazia!");
-        return ResponseEntity.status(200).body(users.stream().map(Mapper::UserMapperDTO).toList());
+        List<UserResponseDTO> usersDTOs = users.getContent().stream().map(user -> modelMapper.map(user, UserResponseDTO.class)).toList();
+        return new PageImpl<>(usersDTOs, pageable, users.getTotalElements());
     }
 
 
     @Override
     public UserResponseDTO getUserById(Long id) {
-        Optional<User> opt = Optional.of(userRepository.findById(id).orElseThrow(() -> {
-            throw new UserByIdNotFound(String.format("Usuário do %d não foi encontrado no sistema", id));
-        }));
-        User user = opt.get();
-        return Mapper.UserMapperDTO(user);
+        Optional<User> opt = userRepository.findById(id);
+        User user = opt.orElseThrow(() -> new UserByIdNotFound(String.format("Usuário do %d não foi encontrado no sistema", id)));
+        return modelMapper.map(user, UserResponseDTO.class);
     }
 
     @Override
-    public ResponseEntity<UserResponseDTO> createUser(UserForm userForm) {
+    public UserResponseDTO createUser(UserForm userForm) {
         Optional<User> op = userRepository.findUserByEmail(userForm.getEmail());
         if (op.isPresent()) throw new ConflictEmail("Email já cadastrado!");
-        User newUser = new User(userForm.getFirstName(), userForm.getLastName(), userForm.getEmail(), userForm.getPassword(), userForm.getRole());
-
+        User newUser = new User();
+        modelMapper.map(userForm, newUser);
         userRepository.save(newUser);
-        return ResponseEntity.status(201).body(Mapper.UserMapperDTO(newUser));
+        return modelMapper.map(newUser, UserResponseDTO.class);
     }
 
     @Override
