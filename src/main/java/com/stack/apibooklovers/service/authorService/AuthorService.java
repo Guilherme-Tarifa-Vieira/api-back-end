150dad31d74a6ba75a6756514b292154db297d11
Guilherme Tarifa Vieira <guilherme.vieira@sptech.school>
2023-10-09 08:38:15 -0300

    refactor: remove method

--- src/main/java/com/stack/apibooklovers/service/authorService/AuthorService.java
+++ src/main/java/com/stack/apibooklovers/service/authorService/AuthorService.java
@@ -1,8 +1,6 @@
 package com.stack.apibooklovers.service.authorService;
 
-import com.stack.apibooklovers.domain.author.Author;
-import org.springframework.http.ResponseEntity;
+
 
 public interface AuthorService {
-    public ResponseEntity<Author> findAuthorById(Long id);
 }
